4eea40c43a0b73eea138ef21b876cff12b42b2c0
Sebastian <sebastian@pop-os.fritz.box>
2025-12-11 21:48:57 +0100

    added file reading and word count

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path,"r") as f:
+        return f.read()
+    
+def count_words(text):
+    number = len(text.split())
+    return number
+
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    num_words = count_words(book_text)
+    print(f"Found {num_words} total words")
+
+main()
