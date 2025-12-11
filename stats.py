4aebfe8c3da5051c072a5a4bb2659ba9f62caec0
Sebastian <sebastian@pop-os.fritz.box>
2025-12-11 22:29:39 +0100

    fixed count each char method

--- stats.py
+++ stats.py
@@ -4,6 +4,9 @@ def count_words(text):
 
 def count_each_char(text):
     letters = {}
-    for char in text.split():
-        letters[char.lower()] += 1
+    for char in text:
+        try:
+          letters[char.lower()] += 1
+        except KeyError:
+          letters[char.lower()] = 1  
     return letters
