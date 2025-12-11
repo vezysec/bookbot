3fe3dbce0b8327ff19402c222b4e47ce841ff541
Sebastian <sebastian@pop-os.fritz.box>
2025-12-11 22:20:48 +0100

    added counting characters feature

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_each_char
 
 def get_book_text(file_path):
     with open(file_path,"r") as f:
@@ -8,6 +8,8 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     num_words = count_words(book_text)
+    char_counts = count_each_char(book_text)
     print(f"Found {num_words} total words")
+    print(char_counts)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,9 @@
 def count_words(text):
     number = len(text.split())
     return number
+
+def count_each_char(text):
+    letters = {}
+    for char in text.split():
+        letters[char.lower()] += 1
+    return letters
