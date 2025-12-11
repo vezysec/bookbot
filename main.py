b606d682d2cc1b88211c29f598cf36b8655eb794
Sebastian <sebastian@pop-os.fritz.box>
2025-12-12 00:14:45 +0100

    fixed sorting bug

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from stats import count_words, count_each_char
+from stats import count_words, count_each_char, sort_letters_by_count
 
 def get_book_text(file_path):
     with open(file_path,"r") as f:
         return f.read()
     
 def main():
+    print("============ BOOKBOT ============")
     book_path = "books/frankenstein.txt"
+    print(f"Analyzing book found at {book_path}...")
     book_text = get_book_text(book_path)
+
+    print("----------- Word Count ----------")
     num_words = count_words(book_text)
-    char_counts = count_each_char(book_text)
     print(f"Found {num_words} total words")
+
+    print("--------- Character Count -------")
+    char_counts = sort_letters_by_count(count_each_char(book_text))
     for char_count in char_counts:
-      print(char_count)
+      if char_count["char"].isalpha():
+        print(f"{char_count["char"]}: {char_count["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -11,13 +11,16 @@ def count_each_char(text):
           letters[char.lower()] = 1  
     return letters
 
+def sort_on_num(item):
+   return item["num"]
+
 def sort_letters_by_count(letters):
    chars_list = []
    for letter in letters:
       char_dict = {"char": letter,"num":letters[letter]}
       chars_list.append(char_dict)
-   return chars_list.sort(reverse=True, key=sort_on_num)
+   chars_list.sort(reverse=True, key=sort_on_num)
+   return chars_list
 
-def sort_on_num(items):
-   return items["num"]
+
       
